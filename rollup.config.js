98d392a0247e8b8cc46c90d85d56adbc98fdcdba
Cheng <mazeyqian@gmail.com>
2026-05-23 17:41:08 +0800

    chore: update configuration files
    Update scripts for consistency and version bump

--- rollup.config.js
+++ rollup.config.js
@@ -1,54 +1,54 @@
-import babel from 'rollup-plugin-babel';
-import rollupTypescript from 'rollup-plugin-typescript2';
-import { DEFAULT_EXTENSIONS } from '@babel/core';
-import commonjs from 'rollup-plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import cleaner from 'rollup-plugin-cleaner';
+import babel from "rollup-plugin-babel";
+import rollupTypescript from "rollup-plugin-typescript2";
+import { DEFAULT_EXTENSIONS } from "@babel/core";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import cleaner from "rollup-plugin-cleaner";
 
 export default [
   {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: [
       {
-        file: 'lib/index.cjs.js',
-        format: 'cjs'
+        file: "lib/index.cjs.js",
+        format: "cjs",
       },
       {
-        file: 'lib/index.esm.js',
-        format: 'esm'
-      }
+        file: "lib/index.esm.js",
+        format: "esm",
+      },
     ],
     plugins: [
       rollupTypescript(),
       commonjs({
-        include: /node_modules/
+        include: /node_modules/,
       }),
       babel({
         runtimeHelpers: true,
         // 只转换源代码，不运行外部依赖
-        exclude: 'node_modules/**',
+        exclude: "node_modules/**",
         // babel 默认不支持 ts 需要手动添加
         extensions: [
           ...DEFAULT_EXTENSIONS,
-          '.ts',
+          ".ts",
         ],
       }),
       cleaner({
         targets: [
-          'lib/',
+          "lib/",
         ],
       }),
     ],
-    external: [ 'mazey', 'copy-to-clipboard' ],
+    external: [ "mazey", "copy-to-clipboard" ],
   },
   {
-    input: 'src/polyfill.js',
+    input: "src/polyfill.js",
     output: { 
-      file: 'lib/polyfill.min.js',
-      format: 'iife',
+      file: "lib/polyfill.min.js",
+      format: "iife",
     },
     plugins: [
-      terser()
+      terser(),
     ],
   },
 ];
